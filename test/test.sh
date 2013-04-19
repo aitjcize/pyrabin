4f9f62c911b6a1f2e0d573020df3418422753b3c
AZ Huang <aitjcize@gmail.com>
2013-04-20 01:26:46 +0800

    Fix test.sh

--- test/test.sh
+++ test/test.sh
@@ -8,19 +8,22 @@ echo -n '[*] Generating a random file "file.bin" ... '
 dd if=/dev/urandom of=file.bin bs=1024 count=100 > /dev/null 2>&1
 echo 'done'
 
-echo -n '[*] Splitting file.bin to rabin chunks ...'
+echo -n '[*] Splitting file.bin to rabin chunks ... '
 python << EOF
+from sys import stdout
 from rabin import split_file_by_fingerprints
 result = split_file_by_fingerprints("file.bin")
 print 'done'
 
-print '[*] Reconstructing "rabin.bin" with rabin chunks ...',
+stdout.write('[*] Reconstructing "rabin.bin" with rabin chunks ...')
 with open('ordered.lst', 'w') as f:
     for start, length, fingerpint, filename in result:
         f.write(filename + "\n")
 EOF
 
 cat `cat ordered.lst` > rabin.bin
+echo 'done'
+
 echo -n '[*] Checking if sha1sum of file.bin and rabin.bin are identical ... '
 sha1_orig=`sha1sum file.bin | awk '{ print $1 }'`
 sha1_recst=`sha1sum rabin.bin | awk '{ print $1 }'`
