ce105ba33f9e100ee38cc42930f5c12f85f13f83
AZ Huang <aitjcize@gmail.com>
2013-05-25 20:41:56 +0800

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 
-setup(name="PyRabin", version="0.1", ext_modules=[
+setup(name="PyRabin", version="0.2", ext_modules=[
     Extension("rabin", [
             "src/pyrabin.c",
             "src/rabin.c",
