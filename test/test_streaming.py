554e29213525adc14f8e2748074130994553a70b
Steve Traugott <stevegt@t7a.org>
2014-10-14 11:52:16 -0700

    finish streaming test

--- test/test_streaming.py
+++ test/test_streaming.py
@@ -7,18 +7,18 @@ import sys
 
 from rabin import Rabin, get_file_fingerprints, set_min_block_size, set_max_block_size, set_average_block_size
 
+stream_count = int(sys.argv[1]) # 100
+seed = int(sys.argv[2]) # $RANDOM
+print 'seed', seed
+
 stream_bs = 1024 * 1024 
-stream_count = 100
 stream_len = stream_bs * stream_count
-max_mem = stream_len/2
+max_mem = 1024 * 1024 * 20
 
 resource.setrlimit(resource.RLIMIT_AS, (max_mem,-1))
 # print resource.getrlimit(resource.RLIMIT_AS)
 
-TARGET = 'test.bin'
-os.system("dd if=/dev/urandom of=%s bs=%d count=%d" % (
-    TARGET, stream_bs, stream_count))
-random.seed(open(TARGET, 'r').read(1024))
+random.seed(seed)
 
 max_blocksize = random.randint(512, max_mem/10)
 
@@ -31,41 +31,42 @@ def block_reached(start, length, fingerprint):
     # print '(%s, %s, %s)' % (start, length, fingerprint)
     reached.append((start, length, fingerprint))
 
+
 r = Rabin()
 r.register(block_reached)
 
-# from guppy import hpy; hp=hpy()
+from guppy import hpy; hp=hpy()
 
-with open(TARGET, 'r') as f:
-    while True:
-        size = random.randint(1,max_blocksize*2)
-        data = f.read(size)
-        if len(data) == 0:
-            break
-        r.update(data)
-        # print hp.heap()
+total = 0
+while total < stream_len:
+    size = random.randint(1,max_blocksize*2)
+    size = min(size, stream_len - total)
+    # print size,
+    data = ''
+    for i in xrange(size):
+        data += chr(random.randrange(0,256))
+    r.update(data)
+    total += len(data)
+    print total
+    print hp.heap()
 
 partial = r.fingerprints()
 print 'partial done'
-gold = get_file_fingerprints(TARGET)
 
-assert len(gold) == len(partial) == len(reached)
+assert len(partial) == len(reached)
 
-for i in range(len(gold)):
+for i in range(len(partial)):
     try:
-        gs,gl,gp = gold[i]
         ps,pl,pp = partial[i]
         rs,rl,rp = reached[i]
-        assert gs == ps == rs
-        assert gl == pl == rl
-        assert gp == pp == rp
+        assert ps == rs
+        assert pl == rl
+        assert pp == rp
     except:
-        print 'gold   ', gold[i]
         print 'partial', partial[i]
         print 'reached', reached[i]
         raise
 
-assert partial == gold == reached
+assert partial == reached
 
-os.unlink(TARGET)
 print 'passed'
