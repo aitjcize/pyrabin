072bad755121b8fe2462f8928f078387942b02dc
Wei-Ning Huang <wnhuang@google.com>
2017-08-18 12:01:53 +0800

    Bump version to 0.6

--- setup.py
+++ setup.py
@@ -1,19 +1,29 @@
 from distutils.core import setup, Extension
 
 setup(
-    name="PyRabin",
-    version="0.5",
+    name='PyRabin',
+    version='0.6',
     description = 'A python for generating rabin fingerprints.',
     author = 'Wei-Ning Huang (AZ)',
     author_email = 'aitjcize@gmail.com',
     url = 'http://github.com/aitjcize/pyrabin',
     license = 'GPL',
     ext_modules=[
-        Extension("rabin", [
-                "src/pyrabin.c",
-                "src/rabin.c",
-                "src/rabin_polynomial.c",
-                "src/utils.c",
+        Extension('rabin', [
+                'src/pyrabin.c',
+                'src/rabin.c',
+                'src/rabin_polynomial.c',
+                'src/utils.c',
        ], libraries=['crypto'])
-    ]
+    ],
+    classifiers = [
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+	'Programming Language :: Python :: 3.3',
+	'Programming Language :: Python :: 3.4',
+	'Programming Language :: Python :: 3.5',
+	'Programming Language :: Python :: 3.6',
+	'Programming Language :: Python :: 3.7',
+    ],
 )
